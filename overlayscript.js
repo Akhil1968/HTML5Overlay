c3db213f4183fc3a7e56cdbd0c7d041a17f2cfc4
akhilanand.singh@wipro.com <handlebars>
2017-07-03 20:06:49 +0530

    first

---
+++ overlayscript.js
@@ -0,0 +1,7 @@
+function spreadOverlay() {
+    document.getElementById("overlay").style.width = "100%";
+}
+
+function closeOverlay() {
+    document.getElementById("overlay").style.width = "0%";
+}
